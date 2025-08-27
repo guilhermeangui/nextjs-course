19612d9080a65f7f793e5c6f8f39d8d02968939e
guilhermeangui <guilhermeangui@live.com>
2025-08-26 22:16:26 -0300

    :pencil: feat: initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: __dirname, // This sets the root to the directory containing this config file
+  },
 };
 
 export default nextConfig;
